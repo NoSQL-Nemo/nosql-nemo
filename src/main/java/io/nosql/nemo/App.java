ff936a9207f7ab1a35d08690d43b41f1bfea29d1
Cédric Maria-Sube <cedric.mariasube@viseo.com>
2021-02-15 10:08:49 +0100

    Adds a basic app.

---
+++ src/main/java/io/nosql/nemo/App.java
@@ -0,0 +1,11 @@
+package io.nosql.nemo;
+
+public class App {
+    public String getGreeting() {
+        return "Hello world.";
+    }
+
+    public static void main(String[] args) {
+        System.out.println(new App().getGreeting());
+    }
+}
